6ae37f719d189e977ec4c10375791a7acb0c1acb
shawnp221 <snpatel@wpi.edu>
2026-02-03 13:42:01 -0500

    More issues fixed with server

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'dist'
+        outDir: 'dist',   // default, but explicit
     },
-    server: {
-        port: 3000,
-        open: true
-    }
-})
+    base: './',         // use relative paths for CSS/JS/assets
+});
